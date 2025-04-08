b4f49131ce444f81ff5c9cef862a7ed2361c287e
JuantMartinez17 <juantomasm0@gmail.com>
2025-04-07 22:50:00 -0300

    [send_messages.py] reads messages to send file correctly and sends messages 1 by 1

--- send_message.py
+++ send_message.py
@@ -1,15 +1,26 @@
 import pywhatkit
-import datetime
+from datetime import datetime
+import pickle
 
-numero = "+5491133292257"
+with open("messages.pkl", "rb") as file:
+    messages = pickle.load(file)
 
-mensaje = "Probando script con libreria pywhatkit"
+for i, (num, msg) in enumerate(messages):
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute + 1
 
-now = datetime.datetime.now()
+    print(f"[{i+1}/{len(messages)}] Enviando mensaje a {num} a las {hour}:{minute:02d}...")
 
-hora_envio = now.hour
-
-min_envio = now.minute + 1
-
-
-pywhatkit.sendwhatmsg(numero, mensaje, hora_envio, min_envio)
+    try:
+        pywhatkit.sendwhatmsg(
+            phone_no=num,
+            message=msg,
+            time_hour=hour,
+            time_min=minute,
+            wait_time=20,
+            tab_close=True
+        )
+        time.sleep(15)
+    except Exception as e:
+        print(f"Error al enviar el mensaje a {num}: {e}")
