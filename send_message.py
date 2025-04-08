73d450b00491fe3ac55bd74a8901c44cd66dd660
Florencia Morelli <florenciamorelliIT@gmail.com>
2025-04-08 00:55:12 -0300

    added def run() to be called by main.py

--- send_message.py
+++ send_message.py
@@ -2,23 +2,24 @@ import pywhatkit
 from datetime import datetime
 import pickle
 
-with open("messages.pkl", "rb") as file:
-    messages = pickle.load(file)
+def run ():
+    with open("messages.pkl", "rb") as file:
+        messages = pickle.load(file)
 
-for i, (num, msg) in enumerate(messages):
-    now = datetime.now()
-    hour = now.hour
-    minute = now.minute + 1
+    for i, (num, msg) in enumerate(messages):
+        now = datetime.now()
+        hour = now.hour
+        minute = now.minute + 1
 
-    print(f"[{i+1}/{len(messages)}] Enviando mensaje a {num} a las {hour}:{minute:02d}...")
+        print(f"[{i+1}/{len(messages)}] Enviando mensaje a {num} a las {hour}:{minute:02d}...")
 
-    try:
-        pywhatkit.sendwhatmsg_instantly(
-            phone_no=num,
-            message=msg,
-            wait_time=5,
-            tab_close=True,
-            close_time=3
-        )
-    except Exception as e:
-        print(f"Error al enviar el mensaje a {num}: {e}")
+        try:
+            pywhatkit.sendwhatmsg_instantly(
+                phone_no=num,
+                message=msg,
+                wait_time=5,
+                tab_close=True,
+                close_time=3
+            )
+        except Exception as e:
+            print(f"Error al enviar el mensaje a {num}: {e}")
